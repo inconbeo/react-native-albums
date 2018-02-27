7a07b6c86cb1061c75386026740f997594b2af4f
Quang Nguyen <qtn2@njit.edu>
2018-02-26 20:06:46 -0500

    working app

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,68 @@
+//Import libraries for making components
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+//Making component
+const AlbumDetail = ({album}) => {
+    //I want title, artist, thumbnail_image from album object
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
+    return (
+    <Card>
+        <CardSection>
+            <View style={thumbnailContainerStyle}>
+                <Image 
+                style={thumbnailStyle}
+                source={{ uri: thumbnail_image }} />
+            </View>
+            <View style={headerContentStyle}>
+                <Text style={headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+        </CardSection>
+
+        <CardSection>
+            <Image 
+            style={imageStyle}
+            source={{ uri: image }} />
+        </CardSection>
+
+        <CardSection>
+            <Button onPress={() => Linking.openURL(url)}>
+                Buy Now
+            </Button>
+        </CardSection>
+    </Card>
+    )
+}
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+}
+
+//Make component available to other parts of the app
+export default AlbumDetail;
---
+++ src/components/AlbumList.js
@@ -0,0 +1,34 @@
+//import library for making component
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
+
+//Make a component (we want to borrow a bunch of functionalities 
+// from the based-component class)
+class AlbumList extends Component {
+    //Set initial state
+    state = { albums: [] };
+
+   componentWillMount () {
+       axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }))
+    } 
+
+    renderAlbums () {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        )
+    }
+render () {
+    console.log(this.state);
+    return (
+        <ScrollView>
+            {this.renderAlbums()} 
+        </ScrollView>
+    );
+    }
+}
+
+// Make the component available to other parts of the app
+export default AlbumList;
